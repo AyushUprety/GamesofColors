13881f5f99f12be19620c04a2a3a0a5ec7f1574a
AyushUprety <ayushuprety1@gmail.com>
2020-08-14 07:47:22 +0545

    Added new functions

--- color.js
+++ color.js
@@ -38,9 +38,9 @@ function fixonecolor()
 }
 
 
-function randomcolor()    // giving random colors to all the boxes.
+function randomcolor(n)    // giving random colors to all the boxes.
 {
-    for(let i=0;i<6;i++)
+    for(let i=0;i<n;i++)
     {
         var store=document.getElementById('box'+i);
         store.style.background='#'+Math.floor(Math.random()*16777215).toString(16);
@@ -52,7 +52,7 @@ var a=document.querySelector('#box0');
 a.addEventListener('click',function()
 {   
     if(this.id!=box.id){
-        a.remove();
+        this.style.backgroundcolor='black';
     }
     else 
     {
@@ -60,8 +60,8 @@ a.addEventListener('click',function()
         for(var i = 0; i < get1.length; i++){
             
            get1[i].style.background=fixonecolor();
-
-        }   
+        }
+        document.querySelector('#tryagain').innerHTML='Correct';   
     }
     
 });
@@ -69,24 +69,23 @@ var b=document.querySelector('#box1');
 b.addEventListener('click',function()
 {
     if(this.id!=box.id){
-        b.remove();
-    }
+        this.style.backgroundColor='black';    }
     else 
     {
         let get1=document.getElementsByClassName('square');  // note that classname returns array so indexing is must to get multiple values.
         for(var i = 0; i < get1.length; i++){                // for that used ge1.length
              
            get1[i].style.background=fixonecolor();
-
-        }       }
+        }
+        document.querySelector('#tryagain').innerHTML='Correct';      
+    }
 });
 
 var c=document.querySelector('#box2');
 c.addEventListener('click',function()
 {
     if(this.id!=box.id){
-        c.remove();
-    }
+        this.style.backgroundColor='black';    }
     else 
     {
         let get1=document.getElementsByClassName('square');
@@ -94,29 +93,15 @@ c.addEventListener('click',function()
             
            get1[i].style.background=fixonecolor();
 
-        }       }
+        } 
+        document.querySelector('#tryagain').innerHTML='Correct';      
+    }
 });
 var d=document.querySelector('#box3');
 d.addEventListener('click',function()
 {
     if(this.id!=box.id){
-        d.remove();
-    }
-    else 
-    {
-        let get1=document.getElementsByClassName('square');
-        for(var i = 0; i < get1.length; i++){
-            
-           get1[i].style.background=fixonecolor();
-
-        }      }
-});
-var e=document.querySelector('#box4');
-e.addEventListener('click',function()
-{
-    if(this.id!=box.id){
-        e.remove();
-        
+        this.style.backgroundColor='black';
     }
     else 
     {
@@ -126,13 +111,15 @@ e.addEventListener('click',function()
            get1[i].style.background=fixonecolor();
 
         }      
-    }
+        document.querySelector('#tryagain').innerHTML='Correct';
+}
 });
-var f=document.querySelector('#box5');
-f.addEventListener('click',function()
+var e=document.querySelector('#box4');
+e.addEventListener('click',function()
 {
     if(this.id!=box.id){
-        f.remove();
+        this.style.backgroundColor='black';
+        
     }
     else 
     {
@@ -141,13 +128,55 @@ f.addEventListener('click',function()
             
            get1[i].style.background=fixonecolor();
 
-        }   
+        }
+        document.querySelector('#tryagain').innerHTML='Correct';      
+    }
+});
+var f=document.querySelector('#box5');
+f.addEventListener('click',function()
+{
+    if(this.id!=box.id){
+        f.style.backgroundColor='black';
+    }
+    else 
+    {
+        let get1=document.getElementsByClassName('square');
+        for(var i = 0; i < get1.length; i++){
+            
+           get1[i].style.background=fixonecolor();
+
+        }
+        document.querySelector('#tryagain').innerHTML='Correct';   
     }
 }); }
+document.querySelector('#Newcolors').addEventListener('click',Newcolors);
+document.querySelector('#easy').addEventListener('click',Easy);
+function Newcolors()
+{   
+    
 
+    generaterandom();
+    randomcolor(6);
+    fixonecolor();
+    rado();
+}
+var square=document.querySelectorAll('.square');
+console.log(square);
+function Easy()
+{
+   
+        for(let i=0;i<6;i++)
+        {
+            square[i].style.backgroundColor='black';
+        }
+        randomcolor(3);
+        fixonecolor();
+
+            
+           
+}   
 
 generaterandom();
-fixonecolor();
-randomcolor();   //All boxex ma random color generate gareko Out of them later one fixed box color is replaced by below function.
-rado();  //selecting and coloring all the boxes with same color if condition is true otherwise deleting it
-
+randomcolor(6);              //All boxex ma random color generate gareko Out of them later one fixed box color is replaced by below function.
+fixonecolor();               // fixing the color for the random box;
+rado();                     //selecting and coloring all the boxes with same color if condition is true otherwise deleting it
